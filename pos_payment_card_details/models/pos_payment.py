4c9329b51c74a1a52eecad8699104f7a4ad846e8
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-03-17 15:54:47 -0300

    fix: removed _load_pos_data_fields from pos_payment.py. The Python fields (lot_number, coupon_number, installments) stay on the model for backend storage

--- pos_payment_card_details/models/pos_payment.py
+++ pos_payment_card_details/models/pos_payment.py
@@ -6,8 +6,3 @@ class PosPayment(models.Model):
     lot_number = fields.Char(string='Lot Number', help='Terminal Lot Number for conciliation')
     coupon_number = fields.Char(string='Coupon Number', help='Terminal Coupon Number for conciliation')
     installments = fields.Integer(string='Installments', help='Number of installments for the payment', default=1)
-
-    def _load_pos_data_fields(self, config_id):
-        fields = super()._load_pos_data_fields(config_id)
-        fields += ['lot_number', 'coupon_number', 'installments']
-        return fields
