e441be09aae0e45ac6c810aab7dafc5755e7e899
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-03-17 16:16:46 -0300

    fix: display total combo price on parent POS order lines.

--- pos_hide_combo_subproduct_prices/__manifest__.py
+++ pos_hide_combo_subproduct_prices/__manifest__.py
@@ -7,6 +7,7 @@
     'data': [],
     'assets': {
         'point_of_sale._assets_pos': [
+            'pos_hide_combo_subproduct_prices/static/src/app/models/pos_orderline.js',
             'pos_hide_combo_subproduct_prices/static/src/app/screens/receipt_screen/receipt/order_receipt.xml',
         ],
     },
