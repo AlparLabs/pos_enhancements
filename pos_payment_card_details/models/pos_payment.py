f06ea0955f7b93e6f68a330262f34904999f68af
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-03-17 16:37:59 -0300

    fix: lot number, coupon number, and installments to POS payment details for backend storage and frontend serialization/printing.

--- pos_payment_card_details/models/pos_payment.py
+++ pos_payment_card_details/models/pos_payment.py
@@ -6,8 +6,3 @@ class PosPayment(models.Model):
     lot_number = fields.Char(string='Lot Number', help='Terminal Lot Number for conciliation')
     coupon_number = fields.Char(string='Coupon Number', help='Terminal Coupon Number for conciliation')
     installments = fields.Integer(string='Installments', help='Number of installments for the payment', default=1)
-
-    def _load_pos_data_fields(self, config_id):
-        data_fields = super()._load_pos_data_fields(config_id)
-        data_fields += ['lot_number', 'coupon_number', 'installments']
-        return data_fields
--- pos_payment_card_details/static/src/app/models/pos_payment_card_details.js
+++ pos_payment_card_details/static/src/app/models/pos_payment_card_details.js
@@ -4,6 +4,13 @@ import { PosPayment } from "@point_of_sale/app/models/pos_payment";
 import { patch } from "@web/core/utils/patch";
 
 patch(PosPayment.prototype, {
+    serialize() {
+        const result = super.serialize ? super.serialize(...arguments) : {};
+        result.lot_number = this.lot_number || "";
+        result.coupon_number = this.coupon_number || "";
+        result.installments = this.installments || 1;
+        return result;
+    },
     export_for_printing() {
         const result = super.export_for_printing(...arguments);
         result.lot_number = this.lot_number || "";
