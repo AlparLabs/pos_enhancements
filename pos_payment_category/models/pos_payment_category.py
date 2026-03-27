3aa468ee26487a1ad15db955c7ee302f36accd2d
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-03-27 01:32:28 -0300

    IMP: POS payment categories to group payment methods for a cleaner UI and improved management.

--- pos_payment_category/models/pos_payment_category.py
+++ pos_payment_category/models/pos_payment_category.py
@@ -11,12 +11,20 @@ class PosPaymentCategory(models.Model):
     sequence = fields.Integer(string='Sequence', default=10, help='Gives the sequence order when displaying a list of payment categories.')
     active = fields.Boolean(default=True)
     color = fields.Integer(string='Color')
-    
+
     payment_method_ids = fields.One2many('pos.payment.method', 'category_id', string='Payment Methods')
+    config_ids = fields.Many2many(
+        'pos.config',
+        'pos_config_payment_category_rel',
+        'category_id', 'config_id',
+        string='Point of Sale',
+        help='Select which POS configurations can use this payment category. If empty, it will not appear in any POS.',
+    )
 
     @api.model
     def _load_pos_data_domain(self, data):
-        return [('active', '=', True)]
+        config_id = data['pos.config']['data'][0]['id']
+        return [('active', '=', True), ('config_ids', 'in', [config_id])]
 
     @api.model
     def _load_pos_data_fields(self, config_id):
@@ -51,4 +59,15 @@ class PosPayment(models.Model):
         related='payment_method_id.category_id',
         string='Payment Category',
         store=True,
+    )
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    payment_category_ids = fields.Many2many(
+        'pos.payment.category',
+        'pos_config_payment_category_rel',
+        'config_id', 'category_id',
+        string='Payment Categories',
+        help='Payment categories available on this POS. Only these categories will appear on the payment screen.',
     )
